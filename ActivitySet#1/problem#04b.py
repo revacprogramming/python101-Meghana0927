689045a5b92b14c1bb7dd0d8078e7377e49e90f0
Meghana0927 <94678051+Meghana0927@users.noreply.github.com>
2022-06-16 16:08:58 +0000

    coded

--- ActivitySet#1/problem#04b.py
+++ ActivitySet#1/problem#04b.py
@@ -1,6 +1,8 @@
 score = input("Enter Score: ")
 s=float(score)
-if s>=0.9:
+if s>=1:
+    print("ERROR")
+elif s>=0.9:
     print("A")
 elif s>=0.8:
     print("B")
--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,2 +1,7 @@
 # Strings
 
+text = "X-DSPAM-Confidence:    0.8475";
+txt = text.find(':')
+number = text[txt+1:]
+n = float(number)
+print(n)
